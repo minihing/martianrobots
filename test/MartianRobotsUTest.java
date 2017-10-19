306d66948fc9fd797b562d294b9ec311ce762c21
minihing <hinghc@gmail.com>
2017-10-20 00:02:05 +0100

    add unit test.

--- test/MartianRobotsUTest.java
+++ test/MartianRobotsUTest.java
@@ -1,4 +1,9 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,10 +15,25 @@ public class MartianRobotsUTest {
             "32N\nFRRFLLFFRRFLL\n\n" +
             "03W\nLLFFFLFLFL";
 
-    private static final String output =
+    private static final String expectedOutput =
             "11E\n" +
             "33NLOST\n" +
             "23S";
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
 
     @Test
     public void parseInput() {
@@ -23,10 +43,11 @@ public class MartianRobotsUTest {
         MartianRobotWorld mars = new MartianRobotWorld(input);
         assertEquals(mars.getWorldWidth(), expectedWorldWidth);
         assertEquals(mars.getWorldHeight(), expectedWorldHeight);
-
+        //more unit tests for other outputs.
     }
     @Test
     public void acceptanceTest() {
-        assertEquals(input, output);
+        new MartianRobotWorld(input).executeCommands();
+        assertEquals(expectedOutput, outContent.toString());
     }
 }
